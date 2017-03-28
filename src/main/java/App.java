08ae498492a278e75829ad00897ff245b81b5944
Mark Fisher and Shane Stafford <shane@>
2017-03-28 16:46:16 -0700

    add route for new doctor addition. Incomplete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,17 +22,30 @@ public class App {
       Specialty newSpecialty = new Specialty(specialtyName);
       newSpecialty.save();
       response.redirect("/");
-      // model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/specialties/:specialtyId", (request, response) ->{
       Map<String, Object> model = new HashMap<String, Object>();
-      // int specialtyId = Integer.parseInt(request.params(":specialtyId"));
-      // Specialty targetSpecialty = Specialty.find(specialtyId);
       Specialty targetSpecialty = Specialty.find(Integer.parseInt(request.params(":specialtyId")));
       model.put("specialty", targetSpecialty);
       model.put("template", "templates/specialty.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/specialties/:specialtyId", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      String doctorName = request.queryParams("doctorName");
+      int specialtyid = Integer.parseInt(request.params(":specialtyId"));
+      Doctor newDoctor = new Doctor(doctorName, specialtyid);
+      newDoctor.save();
+      response.redirect("/specialties/:specialtyId");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/specialties/:specialtyId/doctors/new", (request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/doctor-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
