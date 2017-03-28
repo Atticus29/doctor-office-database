451b95c469cd58bb29e796ff8ca42f9f6681f52f
Mark <mark.aaron.fisher@gmail.com>
2017-03-28 09:35:42 -0700

    add doctor form, doctor vtl, patient form, and skeleton of doctor class

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,9 +1,21 @@
 import org.sql2o.*;
 
 public class Doctor {
+  private String doctorName;
+  private String doctorSpecialty;
 
-  public Doctor() {
-    
+
+  public Doctor(String doctorName, String doctorSpecialty) {
+    this.doctorName = doctorName;
+    this.doctorSpecialty = doctorSpecialty;
+  }
+
+  public String getdoctorName(){
+    return doctorName;
+  }
+
+  public String getDoctorSpecialty(){
+    return doctorSpecialty;
   }
 
 }
